4683f60b0f6ec04a7b4a8e8d5e889eee0373a0a7
quantroc21 <quantroc2212@gmail.com>
2026-02-06 18:28:41 +0700

    fix: v15.18 fix compilation block indentation and revert cache dir

--- fish_speech/models/text2semantic/inference.py
+++ fish_speech/models/text2semantic/inference.py
@@ -691,14 +691,14 @@ def load_model(checkpoint_path, device, precision, compile=False, is_agent=False
         )
         logger.info("Using NaiveTransformer")
 
-        if compile:
-            logger.info("Compiling function...")
-            decode_one_token = torch.compile(
-                decode_one_token,
-                fullgraph=True,
-                backend="inductor" if torch.cuda.is_available() else "aot_eager",
-                mode="reduce-overhead" if torch.cuda.is_available() else None,
-            )
+    if compile:
+        logger.info("Compiling function...")
+        decode_one_token = torch.compile(
+            decode_one_token,
+            fullgraph=True,
+            backend="inductor" if torch.cuda.is_available() else "aot_eager",
+            mode="reduce-overhead" if torch.cuda.is_available() else None,
+        )
 
     return model.eval(), decode_one_token
 
